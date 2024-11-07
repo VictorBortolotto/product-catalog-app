b6a75b5106f15593b63539b42eb8810407050b82
VictorBortolotto <victorcamposbortolotto@gmail.com>
2024-11-07 03:21:29 -0300

    Adjust product service

--- frontend/src/app/service/product/product.service.ts
+++ frontend/src/app/service/product/product.service.ts
@@ -13,42 +13,59 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   save(product: Product): Observable<Product> {
+    let token = this.getToken();
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
     });
 
     return this.http.post<Product>(`${this.url}/product`, product, { headers });
   }
 
   update(product: Product): Observable<Product> {
+    let token = this.getToken();
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
     });
 
     return this.http.post<Product>(`${this.url}/product`, product, { headers });
   }
 
   get(id: number): Observable<Product> {
+    let token = this.getToken();
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
     });
 
     return this.http.get<Product>(`${this.url}/product/` + id, {headers});
   }
 
   getAll(): Observable<Array<Product>> {
+    let token = this.getToken();
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
     });
 
     return this.http.get<Array<Product>>(`${this.url}/product`, {headers});
   }
 
   delete(id: number): Observable<any> {
+    let token = this.getToken();
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
     });
 
     return this.http.delete(`${this.url}/product/` + id, {headers});
+  }
+
+  getToken(): string | null {
+    if (typeof window !== 'undefined' && localStorage) {
+      return localStorage.getItem('token');
+    }
+    return null;
   }
 }
