8a385458895327751c5a9aebfd205a2b972eeaf5
VictorBortolotto <victorcamposbortolotto@gmail.com>
2024-11-07 03:12:33 -0300

    Create a user repository

--- backend/src/main/java/br/com/example/api/repository/UserRepository.java
+++ backend/src/main/java/br/com/example/api/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package br.com.example.api.repository;
 
-public interface UserRepository {
+import br.com.example.api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(@Param("email") String email);
+    Boolean existsByEmail(@Param("email") String email);
 }
