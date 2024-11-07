3dac0df037106c5ab0c3cb66ac3bc2be8f9de3ee
VictorBortolotto <victorcamposbortolotto@gmail.com>
2024-11-07 03:18:04 -0300

    Create UserDetails

--- backend/src/main/java/br/com/example/api/auth/authServices/UserDetailsServiceImpl.java
+++ backend/src/main/java/br/com/example/api/auth/authServices/UserDetailsServiceImpl.java
@@ -1,4 +1,21 @@
 package br.com.example.api.auth.authServices;
 
-public class UserDetailsServiceImpl {
+import br.com.example.api.model.User;
+import br.com.example.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+        return UserDetailsImpl.build(user);
+    }
 }
