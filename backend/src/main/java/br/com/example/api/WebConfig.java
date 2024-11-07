de698050860f3db5d48484bdcb564a03c93c0610
VictorBortolotto <victorcamposbortolotto@gmail.com>
2024-11-07 03:16:48 -0300

    Adjust webconfig

--- backend/src/main/java/br/com/example/api/WebConfig.java
+++ backend/src/main/java/br/com/example/api/WebConfig.java
@@ -8,6 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
-        corsRegistry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
