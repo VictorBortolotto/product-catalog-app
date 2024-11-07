25a8d8a669b7f83fc804f2e10269f110d51f649b
VictorBortolotto <victorcamposbortolotto@gmail.com>
2024-11-06 23:26:56 -0300

    Create WebConfig

--- backend/src/main/java/br/com/example/api/WebConfig.java
+++ backend/src/main/java/br/com/example/api/WebConfig.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+package br.com.example.api;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+    }
 }
