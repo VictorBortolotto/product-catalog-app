8299e6f05b15a67ecaa27b6b8d2ad78a1eba326d
VictorBortolotto <victorcamposbortolotto@gmail.com>
2024-11-06 23:28:34 -0300

    Create ProductCategoryRepository

--- backend/src/main/java/br/com/example/api/repository/ProductCategoryRepository.java
+++ backend/src/main/java/br/com/example/api/repository/ProductCategoryRepository.java
@@ -1,4 +1,21 @@
 package br.com.example.api.repository;
 
-public class ProductCategoryRepository {
+import br.com.example.api.model.ProductCategory;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+    List<ProductCategory> findByCategoryId(long categoryId);
+    List<ProductCategory> findByProductId(long productId);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM ProductCategory pc WHERE pc.category.id = :categoryId AND pc.product.id = :productId")
+    void deleteByCategoryIdAndProductId(long categoryId, long productId);
 }
