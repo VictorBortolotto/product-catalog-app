6b5d7ab2c0d6be8150904628cc906ca89932484e
VictorBortolotto <victorcamposbortolotto@gmail.com>
2024-11-06 23:27:59 -0300

    Create ProductCategory class

--- backend/src/main/java/br/com/example/api/model/ProductCategory.java
+++ backend/src/main/java/br/com/example/api/model/ProductCategory.java
@@ -1,4 +1,75 @@
 package br.com.example.api.model;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "product_category")
 public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    public ProductCategory() {}
+
+    public ProductCategory(Category category, Product product) {
+        this.category = category;
+        this.product = product;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductCategory that = (ProductCategory) o;
+        return Objects.equals(id, that.id) && Objects.equals(category, that.category) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, category, product);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductCategory{" +
+                "id=" + id +
+                ", category=" + category +
+                ", product=" + product +
+                '}';
+    }
 }
