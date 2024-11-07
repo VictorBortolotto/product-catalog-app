0bdacb6bec3e6127ca24c5c6284e980a07afbbed
VictorBortolotto <victorcamposbortolotto@gmail.com>
2024-11-06 23:39:26 -0300

    Create categories component

---
+++ frontend/src/app/pages/categories/categories.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { Category } from '../../model/Category.model';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CategoryService } from '../../service/category/category.service';
+import { ToastService } from '../../service/toast/toast.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-categories',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.css'
+})
+export class CategoriesComponent implements OnInit {
+  @ViewChild('toastContainer', { read: ViewContainerRef }) toastContainer!: ViewContainerRef;
+  form!: FormGroup;
+  categories: Category[] = [];
+
+  constructor(private http: CategoryService, private fb: FormBuilder, private toastService: ToastService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.http.getAll().subscribe(response => {
+      this.categories = response
+    });
+  }
+  
+  ngAfterViewInit(): void {
+    if (this.toastContainer) this.toastService.setViewContainerRef(this.toastContainer);
+  }
+
+  onFabClick() {
+    this.router.navigate(['/category'])
+  }
+
+  onClickDelete(category: Category) {
+    if (category.id) {
+      this.http.delete(category.id).subscribe({
+        next: () => {
+          this.categories = this.categories.filter(c => c.id !== category.id);
+          this.toastService.showToast("Category deleted successfully!", "green")
+        },
+        error: () => {
+          this.toastService.showToast("Failed to delete the category. Please try again.", "red")
+        }
+      });
+    }
+  }
+
+  onClickEdit(category: Category) {
+    if (category.id) {
+      this.router.navigate(['/category', category.id], { state: { data: category } })
+    }
+  }
+}
